cc281c59252501cad95dda7adffc4b7e51922d40
s07352029 <43977864+s07352029@users.noreply.github.com>
2018-10-24 19:57:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,9 +42,6 @@ def callback():
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text='Nice to meet you!')
-    line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
