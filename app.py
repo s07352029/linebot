e073543aec5faf9e901864974df6b9b5c8b971ea
s07352029 <43977864+s07352029@users.noreply.github.com>
2018-12-26 15:55:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def getCls(cls_prefix):
 # 爬搜尋引擎，預設爬回傳4筆
 def get_search_engine(search_thing, result_num=4):
     result = []
-    target_url = 'https://www.bing.com/search'
+    target_url = 'https://www.google.com.tw/search'
     target_param = urllib.parse.urlencode({'q':search_thing}) # Line bot 所接收的關鍵字 !!!!
     target = target_url + '?' + target_param
     r = requests.get(target)
