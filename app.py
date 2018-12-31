0d88813485c8d193d064e48fbc82c22387890d2e
s07352029 <43977864+s07352029@users.noreply.github.com>
2019-01-01 02:30:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
      msg=event.message.text
-     print(msg)
      line_bot_api.reply_message(event.reply_token,TextSendMessage(text='倒數'))
 
 def find_bookls(kw):
