b175dd4303f5f8fe793288384a6e4188011356e8
s07352029 <43977864+s07352029@users.noreply.github.com>
2019-01-01 02:53:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
      if '右轉' in _token[0] or '左轉' in _token[0]:
-            msg=event.message.text
+            messenge=ms(_token[1])
+            for msg in messnge:
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='收到'))
   
 
