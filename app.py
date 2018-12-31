3dcf846bdb50a8da4d80cab1f14f36954e610aaf
s07352029 <43977864+s07352029@users.noreply.github.com>
2019-01-01 02:59:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,11 +49,10 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-     if '右轉' in _token[0] or '左轉' in _token[0]:
-            messenge=ms(_token[1])
-            msg=event.message.text
-            for msg in messnge:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='收到'))
+    msg=event.message.text
+    if msg=='右轉'or'左轉':
+        print('收到')
+        #line_bot_api.reply_message(event.reply_token,TextSendMessage(text='收到'))
   
 
 def find_bookls(kw):
