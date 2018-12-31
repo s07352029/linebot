e86b77cf7ef5d08fb2ccc8a50a1b20056d21a5c5
s07352029 <43977864+s07352029@users.noreply.github.com>
2019-01-01 02:35:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
      msg=event.message.text
-     line_bot_api.reply_message(event.reply_token,TextSendMessage(text='倒數'))
+     if msg=='右轉'or'左轉'
+     line_bot_api.reply_message(event.reply_token,TextSendMessage(text='收到'))
+  
 
 def find_bookls(kw):
     with open("ESLITE.json",'r') as load_f:
